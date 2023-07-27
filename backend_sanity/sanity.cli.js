35dc457cac68620dba555cbfd194d05b89723e76
kennethogrey <ogirekenneth@gmail.com>
2023-07-27 13:45:08 +0300

    first commit =>finished setup and navbar

---
+++ backend_sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '73ewyigc',
+    dataset: 'production'
+  }
+})
---
+++ frontend_react/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import {Navbar} from './components'
+import './App.scss'
+import { About, Footer, Header, Skills, Testimonial, Work} from './container'
+
+const App = () =>{
+    return (
+        <div className="app">
+            <Navbar/>
+            <Header/>
+            <About/>
+            <Work/>
+            <Skills/>
+            <Testimonial/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default App;
